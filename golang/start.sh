b44196cb3fffb5225b67cc90305e48b2ee954885
86shin <hermano7ryo@gmail.com>
2025-12-12 16:09:05 +0900

    [fix] マルチステージングビルド

--- golang/start.sh
+++ golang/start.sh
@@ -3,3 +3,5 @@ if [ ! -f go.mod ]; then
   go mod init github.com/86shin/commit_goback
 fi
 tail -f /dev/null
+
+go run cmd/main.go
--- nodejs/start.sh
+++ nodejs/start.sh
@@ -3,3 +3,5 @@ if [ ! -f package.json ]; then
   npm init -y >/dev/null 2>&1
 fi
 tail -f /dev/null
+
+npm start
