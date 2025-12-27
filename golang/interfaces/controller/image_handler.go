6283ea1297bc033a4519d1526a90fc6842a961e5
86shin <hermano7ryo@gmail.com>
2025-12-27 23:00:22 +0900

    [fix] golangのアドバイス処理に修正

--- golang/interfaces/controller/image_handler.go
+++ golang/interfaces/controller/image_handler.go
@@ -33,9 +33,10 @@ func (h *ImageHandler) AnalyzeImageEchoHandler(c echo.Context) error {
 	var req dto.ImageRequest
 	if err := c.Bind(&req); err != nil {
 		return c.JSON(http.StatusBadRequest, dto.AnalysisResponse{
-			Status: "400",
+			Status:  "400",
+			Message: "無効なリクエストフォーマットです",
 			Analysis: &model.CompositionAnalysis{
-				Advice:   "無効なリクエストフォーマットです",
+				Advice:   err.Error(),
 				Category: req.Category, // 空文字列
 			},
 		})
@@ -43,15 +44,17 @@ func (h *ImageHandler) AnalyzeImageEchoHandler(c echo.Context) error {
 
 	// 2. アプリケーション層（Usecase）への処理委譲
 	// ここで string ではなく、構造体 (AnalysisResult) が返ってくるように実装します
-	analysisResult, err := h.Analyzer.AnalyzeImage(ctx, req.Category, req.Base64Image, req.MimeType)
+	analysisResult, err := h.Analyzer.AnalyzeImage(ctx, req.UserId, req.Category, req.Base64Image, req.MimeType,
+		req.Geo, req.Lat, req.Lng)
 
 	if err != nil {
 		log.Printf("[Analysis Error] %v", err)
 		// エラー時のフォールバック（空の構造体にエラーメッセージだけ入れるなど）
-		return c.JSON(http.StatusOK, dto.AnalysisResponse{
-			Status: "500", // またはエラーを示すコード
+		return c.JSON(http.StatusInternalServerError, dto.AnalysisResponse{
+			Status:  "500", // またはエラーを示すコード
+			Message: "写真の構図に関するアドバイスを取得できませんでした。",
 			Analysis: &model.CompositionAnalysis{
-				Advice:   "写真の構図に関するアドバイスを取得できませんでした。",
+				Advice:   err.Error(),
 				Category: req.Category, // 空文字列
 			},
 		})
@@ -60,6 +63,7 @@ func (h *ImageHandler) AnalyzeImageEchoHandler(c echo.Context) error {
 	// 3. レスポンスの整形と返却
 	res := dto.AnalysisResponse{
 		Status:   "200",
+		Message:  "写真の構図に関するアドバイスを取得しました。",
 		Analysis: analysisResult, // ポインタで受け取った場合は実体を入れる
 	}
 
