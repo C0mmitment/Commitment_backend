414649a9a24ddf3ea950787dca46448979f0ed23
kinoshita <171928933+yukikinoshita09@users.noreply.github.com>
2025-12-28 23:18:16 +0900

    APIテスト用修正

--- nodejs/routes/routes.mjs
+++ nodejs/routes/routes.mjs
@@ -11,7 +11,7 @@ middle.post('/advice', uploadSinglePhoto, handler.advice);
 router.use('/v1', v1);
 v1.use('/middle', middle);
 v1.get('/gtest', handler.gtest)
-v1.post('ptest', handler.ptest)
+v1.post('/ptest', handler.ptest)
 v1.get('/deleteLocationData/:uuid',handler.deleteLocationData)
 v1.get('/heatmap/:geohash',handler.heatmapData);
 
