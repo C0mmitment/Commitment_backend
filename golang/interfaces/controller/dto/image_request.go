50be3d95c36a892b4b4df9a96d7a0975dcb3810e
86shin <hermano7ryo@gmail.com>
2026-01-06 01:28:50 +0900

    [fix] null対応

--- golang/interfaces/controller/dto/image_request.go
+++ golang/interfaces/controller/dto/image_request.go
@@ -7,8 +7,8 @@ type ImageRequest struct {
 	Category    string    `json:"category"`
 	Base64Image string    `json:"image_data_base64"`
 	MimeType    string    `json:"mime_type"`
-	Lat         float64   `json:"latitude"`
-	Lng         float64   `json:"longitude"`
+	Lat         *float64  `json:"latitude"`
+	Lng         *float64  `json:"longitude"`
 	Geo         string    `json:"geohash"`
 	SaveLoc     bool      `json:"save_loc"`
 }
